26b913386c3e65982c3b94f6c44ef54d4e6c927a
asus <asus@LAPTOP-5RR1SHUE>
2023-10-23 01:28:43 +0800

    Add project files.

---
+++ Piloton_WaterBill/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Piloton_WaterBill
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the previous bill: ");
+            double previousbill = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the current bill: ");
+            double currentbill = double.Parse(Console.ReadLine());
+
+            double gallonsUsed = currentbill - previousbill;
+            double totalCharge;
+
+            if ((gallonsUsed / 1000.0) * 7 < 16.67)
+            {
+                totalCharge = 16.67;
+            }
+            else
+            {
+                totalCharge = (gallonsUsed / 1000.0) * 7;
+            }
+            Console.WriteLine("Number of gallons used: " + gallonsUsed);
+            Console.WriteLine("Total charge for meter: " + totalCharge.ToString("F2") + "pesos");
+        }
+    }
+}
